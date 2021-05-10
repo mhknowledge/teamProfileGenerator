cbf41bd3212e1f22dd665c3138af070b45e0a567
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-10 00:37:29 -0400

    test file

---
+++ test/Engineer.test.js
@@ -0,0 +1,23 @@
+//Engineer constructor 
+const Engineer = require('./lib/Engineer');
+
+// creating engineer   
+test('creates an Engineer', () => {
+    const engineer = new Engineer('Muhammed', 40, 'mhknowledge@yahoo.com', 'mhknowledge');
+    
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+// test for getGithub()
+test('gets engineer github', () => {
+    const engineer = new Engineer('Muhammed', 40, 'mhknowledge@yahoo.com', 'mhknowledge');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+//test getRole() 
+test('gets role of employee', () => {
+    const engineer = new Engineer('Muhammed', 40, 'mhknowledge@yahoo.com', 'mhknowledge');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
