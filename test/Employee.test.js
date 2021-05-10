9ef98cada2954c05bb97ebe71baa154a5392388b
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-10 00:20:32 -0400

    uploading test for Employee constructor

---
+++ test/Employee.test.js
@@ -0,0 +1,26 @@
+const Employee = required('./lib/Employee');
+
+test('creates an employee', () => {
+    const employee = new Employee('Muhammed', 40, 'mhknowledge@yahoo.com');
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+//test getID();
+test('prompts for employee id', () => {
+    const employee = new Employee('Muhammed', 40, 'mhknowledge@yahoo.com');
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
+
+//test getEmail();
+test('prompts for employee email', () => {
+    const employee = new Employee('Muhammed', 40, 'mhknowledge@yahoo.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+//test getRole();
+test('prompts user to choose a role', () => {
+    const employee = new Employee('Muhammed', 40, 'mhknowledge@yahoo.com');
+    expect(employee.getRole()).toEqual('Employee');
+});
